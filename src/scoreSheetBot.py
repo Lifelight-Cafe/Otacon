96f770718f9564607836a6111c5ee08d5dc31dfa
superhylia | Levi Ireri <43157926+superhylia@users.noreply.github.com>
2025-06-08 00:07:46 -0500

    Update scoreSheetBot.py without usage to test

--- src/scoreSheetBot.py
+++ src/scoreSheetBot.py
@@ -90,7 +90,7 @@ class ScoreSheetBot(commands.Cog):
                                 if staff or not cmd.hidden:
                                     halp = discord.Embed(title=group[0],
                                                          description=f'{cmd.description}\n'
-                                                                     f'{cmd.usage}\n')
+                                                                     f'{cmd.description}\n')
                                 else:
                                     await ctx.author.send('That command is hidden.')
                             found = True
